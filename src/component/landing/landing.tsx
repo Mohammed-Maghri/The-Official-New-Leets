4eb273ebf8c0a7826788b363d355b1ceffad5140
Mohammed maghri <mohammedmaghri22@gmail.com>
2025-07-14 19:16:17 +0100

    Pushing

--- src/component/landing/landing.tsx
+++ src/component/landing/landing.tsx
@@ -55,8 +55,21 @@ const LandingComponent: FC = () => {
             <span className="font-Tektur">Access Old Version</span>
           </div>
         </button>
+        <button
+          onClick={() => window.open("https://github.com/Mohammed-Maghri", "_blank")}
+          className="cursor-pointer w-full bg-gradient-to-r border-solid border-[1px] border-gray-500/40 from-gray-900/20
+             to-gray-800/20 text-gray-200 py-2 px-4 rounded-md font-light text-xs shadow-md transition-all 
+             duration-300 hover:scale-105 hover:from-gray-800/30 hover:to-gray-700/30 hover:shadow-gray-500/20"
+        >
+          <div className="flex items-center justify-center space-x-2">
+            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z"/>
+            </svg>
+            <span className="font-Tektur">Drop a Follow</span>
+          </div>
+        </button>
       </div>
-      <div className="w-[200px] z-20 text-center space-y-3 pt-4 border-t border-blue-600/30">
+      <div className="w-[200px] z-20 text-center space-y-3 pt-2 border-t border-blue-600/30 mt-2">
         <p className="text-blue-300/70 text-xs font-Tektur">
           Not feeling good with the update?
         </p>
